ac627be0303bdb611fb04ae72f0f72afa4d3372b
Navan Chauhan <navanchauhan@gmail.com>
2024-04-17 15:25:27 -0600

    add tests for Player

---
+++ Tests/SwiftChessNeoTests/SwiftChessNeoPlayer.swift
@@ -0,0 +1,30 @@
+//
+//  SwiftChessNeoPlayer.swift
+//  
+//
+//  Created by Navan Chauhan on 4/17/24.
+//
+
+import XCTest
+@testable import SwiftChessNeo
+
+final class SwiftChessNeoPlayer: XCTestCase {
+    
+    func testPlayerStruct() {
+        let player1 = Player(kind: .human, name: "Magnus Carlsen", elo: 2900)
+        let player2 = Player(kind: .human, name: "Magnus Carlsen", elo: 2900)
+        let player3 = Player(kind: .computer, name: "Magnot Carlsen", elo: 2900)
+        
+        XCTAssertEqual(player1.kind.isHuman, true)
+        XCTAssertEqual(player1.kind.isComputer, false)
+        XCTAssertEqual(player3.kind.isHuman, false)
+        XCTAssertEqual(player3.kind.isComputer, true)
+        XCTAssertEqual(player1, player2)
+        
+        XCTAssertEqual(player1.kind.description, "Human")
+        XCTAssertEqual(player3.kind.description, "Computer")
+        
+        XCTAssertEqual(player1.description, "Player(kind: Human, name: Magnus Carlsen, elo: 2900)")
+    }
+
+}
